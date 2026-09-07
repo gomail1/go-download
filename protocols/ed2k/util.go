e51d71f2f7dff1efa17c416c3cd8b62b6168a003
gomail1 <626536@qq.com>
2026-09-07 21:57:00 +0800

    移除

--- protocols/ed2k/util.go
+++
@@ -1,7 +0,0 @@
-package ed2k
-
-import "encoding/hex"
-
-func hexEncode(b []byte) string { return hex.EncodeToString(b) }
-
-func hexDecodeHash(s string) ([]byte, error) { return hex.DecodeString(s) }
